d7819a3f54375e6f2275704d948d34dc85dd6967
Evgeny Popov <evgeny.popov@eastbanctech.com>
2016-04-01 00:17:09 -0700

    options and bat

---
+++ bin/windows/csharp-hostme-all.bat
@@ -0,0 +1,10 @@
+set executable=.\modules\swagger-codegen-cli\target\swagger-codegen-cli.jar
+
+If Not Exist %executable% (
+  mvn clean package
+)
+
+set JAVA_OPTS=%JAVA_OPTS% -XX:MaxPermSize=256M -Xmx1024M -DloggerPath=conf/log4j.properties
+set ags=generate -t modules\swagger-codegen\src\main\resources\csharp -i http://hostme-services-dev.azurewebsites.net:80/swagger/docs/all -l csharp -o client\all\csharp -c options.json
+
+java %JAVA_OPTS% -jar %executable% %ags%
