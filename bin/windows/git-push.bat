ea2fa65ee4b038800f2e6b137dc1110cd5cb783c
Evgeny Popov <evgeny.popov@eastbanctech.com>
2016-04-01 11:57:41 -0700

    fix

--- bin/windows/git-push.bat
+++ bin/windows/git-push.bat
@@ -1,3 +1,7 @@
-git init %1
-git add %1/git-push.bat
+cd /d %1
+git init
+git add .
 git commit -m "First commit"
+git remote add origin https://github.com/HostMeApp/Sdk.git
+git remote -v
+git push origin master
