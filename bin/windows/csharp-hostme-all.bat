ecd1e9c2b760c1eec4eb00d5f8ab3f0599d19663
Evgeny Popov <evgeny.popov@eastbanctech.com>
2016-04-07 11:34:55 -0700

    fine tuning

--- bin/windows/csharp-hostme-all.bat
+++ bin/windows/csharp-hostme-all.bat
@@ -7,9 +7,9 @@ If Not Exist %executable% (
 set tempate_dir=..\..\modules\swagger-codegen\target\classes
 set language=csharp
 set JAVA_OPTS=%JAVA_OPTS% -XX:MaxPermSize=256M -Xmx1024M -DloggerPath=conf/log4j.properties
-set ags=generate --api-package HostMe.Sdk.Admin.Apis --model-package HostMe.Sdk.Models -t %template_dir%\%language% -i http://hostme-services-dev.azurewebsites.net:80/swagger/docs/all -l %language% -o samples\client\petstore\%language%
-set ags1=generate --api-package HostMe.Sdk.Mobile.Apis --model-package HostMe.Sdk.Models -t %template_dir%\%language% -i http://hostme-services-dev.azurewebsites.net:80/swagger/docs/mb -l %language% -o samples\client\petstore\%language%
-set ags2=generate --api-package HostMe.Sdk.Mobile.Apis --model-package HostMe.Sdk.Models -t %template_dir%\%language% -i http://hostme-services-dev.azurewebsites.net:80/swagger/docs/mb -l %language% -o samples\client\petstore\%language%
+set ags=generate  --model-package HostMe.Sdk.Models -t %template_dir%\%language% -i http://hostme-services-dev.azurewebsites.net:80/swagger/docs/all -l %language% -o samples\client\petstore\%language%
+set ags1=generate --api-package HostMe.Sdk.Apis.Mobile --model-package HostMe.Sdk.Models -t %template_dir%\%language% -i http://hostme-services-dev.azurewebsites.net:80/swagger/docs/mb -l %language% -o samples\client\petstore\%language%
+set ags2=generate --api-package HostMe.Sdk.Apis.Admin --model-package HostMe.Sdk.Models -t %template_dir%\%language% -i http://hostme-services-dev.azurewebsites.net:80/swagger/docs/mb -l %language% -o samples\client\petstore\%language%
 
 
 java %JAVA_OPTS% -Dmodels -DsupportingFiles -jar %executable% %ags% -c options.json
