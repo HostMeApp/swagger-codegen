4c6af4b41cd2f28d2693a84240dd9e4175b3a88b
Evgeny Popov <evgeny.popov@eastbanctech.com>
2016-05-18 12:43:25 -0700

    'changes'

--- cmd/sdk.bat
+++ cmd/sdk.bat
@@ -49,17 +49,17 @@ git add .
 git commit -m %release_note%
 git push
 
-cd /d ..\hostme-sdk-angular-web
+cd /d %out_dir%\hostme-sdk-angular-web
 git add .
 git commit -m %release_note%
 git push
 
-cd /d ..\hostme-sdk-angular-admin
+cd /d %out_dir%\hostme-sdk-angular-admin
 git add .
 git commit -m %release_note%
 git push
 
-cd /d ..\hostme-sdk-csharp
+cd /d %out_dir%\hostme-sdk-csharp
 git add .
 git commit -m %release_note%
 git push
