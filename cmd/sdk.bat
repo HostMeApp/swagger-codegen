f249cfc72fb4952ae9a4ed4462bcdf60d6e057d9
Evgeny Popov <evgeny.popov@eastbanctech.com>
2016-05-19 01:29:21 -0700

    version

--- cmd/sdk.bat
+++ cmd/sdk.bat
@@ -8,6 +8,7 @@ set swagger_uri=%1
 set git_repo_url=%2
 set release_note=%3
 set out_dir=clients
+set version=%4
 
 git clone %git_repo_url%/hostme-sdk-angular-mobile %out_dir%/hostme-sdk-angular-mobile
 git clone %git_repo_url%/hostme-sdk-angular-web %out_dir%/hostme-sdk-angular-web
@@ -48,19 +49,19 @@ cd /d %out_dir%\hostme-sdk-angular-mobile
 git add .
 git commit -m %release_note%
 call npm install
-call npm version
+call npm version %version%
 
 cd /d ..\hostme-sdk-angular-web
 git add .
 git commit -m %release_note%
 call npm install
-call npm version
+call npm version %version%
 
 cd /d ..\hostme-sdk-angular-admin
 git add .
 git commit -m %release_note%
 call npm install
-call npm version
+call npm version %version%
 
 cd /d ..\hostme-sdk-csharp
 git add .
--- cmd/sdk1.bat
+++ cmd/sdk1.bat
@@ -12,32 +12,22 @@ set out_dir=clients
 git clone %git_repo_url%/hostme-sdk-angular-mobile %out_dir%/hostme-sdk-angular-mobile
 git clone %git_repo_url%/hostme-sdk-angular-web %out_dir%/hostme-sdk-angular-web
 git clone %git_repo_url%/hostme-sdk-angular-admin %out_dir%/hostme-sdk-angular-admin
-git clone %git_repo_url%/hostme-sdk-csharp %out_dir%/hostme-sdk-csharp
-
 
 cd /d %out_dir%\hostme-sdk-angular-mobile
 git add .
+git commit -m %release_note%
+call npm version
 call npm install
-call npm run version -- -b %release_note%
-git push
-git push --tags
+call npm version
 
 cd /d ..\hostme-sdk-angular-web
 git add .
+git commit -m %release_note%
 call npm install
-call npm run version -- -b %release_note%
-git push
-git push --tags
+call npm version
 
 cd /d ..\hostme-sdk-angular-admin
 git add .
-call npm install
-call npm run version -- -b %release_note%
-git push
-git push --tags
-
-cd /d ..\hostme-sdk-csharp
-git add .
 git commit -m %release_note%
-git push
-git push --tags
+call npm install
+call npm version
